9d66aae16a394c0aacefd7e3783fd248c16be137
S00123318 <S00123318@VLAB-KSODVY2-34>
2020-05-13 10:16:37 +0100

    Properities

---
+++ DaleDuncan_S00123318/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaleDuncan_S00123318
+{
+    class Phone
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Operating { get; set; }
+
+        public string OS_Image { get; set; }
+
+        public string phone_image { get; set; }
+
+
+
+    }
+
+}
